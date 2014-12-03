d393794f7d35c386a185b1362d610bc37a890afc
Brett Wilkins <brett@brett.geek.nz>
2014-12-03 22:57:03 +1100

    Converted to glfw3 and glow-generated bindings and YAY IT WORKS!!!

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
   "log"
-  "github.com/go-gl/gl"
-  "github.com/go-gl/glfw"
+  "github.com/go-gl/glow/gl-core/3.3/gl"
+  glfw "github.com/go-gl/glfw3"
   "io/ioutil"
+  "strings"
 )
 
 type Screen struct {
@@ -12,136 +13,147 @@ type Screen struct {
   h int
 }
 
-func LoadShader(filename string, shader_type gl.GLenum) (shader gl.Shader) {
+func LoadShader(filename string, shader_type uint32) (shader uint32) {
   var shader_bytes []byte
+  var shader_string string
   var shader_err error
+  var status int32
   shader = gl.CreateShader(shader_type)
   shader_bytes, shader_err = ioutil.ReadFile(filename)
+  shader_bytes = append(shader_bytes, []byte("\x00")[0])
   if shader_err != nil {
     log.Fatal("Could not load shader from file: ", filename)
   }
 
-  shader.Source(string(shader_bytes))
-  shader.Compile()
+  shader_string = string(shader_bytes)
+  csource := gl.Str(shader_string)
+  gl.ShaderSource(shader, 1, &csource, nil)
+  gl.CompileShader(shader)
 
-  if shader.Get(gl.COMPILE_STATUS) == gl.FALSE {
+  gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+  if status == gl.FALSE {
     log.Printf("Compile error in shader %s:\n", filename)
-    log.Fatal(shader.GetInfoLog())
+    var logLength int32
+    gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+    l := strings.Repeat("\x00", int(logLength+1))
+    gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(l))
+
+    log.Fatal(l)
   }
 
   return
 }
 
-func LoadShaderProgram() (program gl.Program) {
-  var vertex_shader, fragment_shader gl.Shader
+func LoadShaderProgram() (program uint32) {
+  var vertex_shader, fragment_shader uint32
 
 
   program = gl.CreateProgram()
 
   vertex_shader = LoadShader("vertex_shader.txt", gl.VERTEX_SHADER)
   fragment_shader = LoadShader("fragment_shader.txt", gl.FRAGMENT_SHADER)
-  program.AttachShader(vertex_shader)
-  program.AttachShader(fragment_shader)
+  gl.AttachShader(program, vertex_shader)
+  gl.AttachShader(program, fragment_shader)
 
-  program.Link()
+  gl.LinkProgram(program)
 
-  program.DetachShader(vertex_shader)
-  program.DetachShader(fragment_shader)
+  gl.DetachShader(program, vertex_shader)
+  gl.DetachShader(program, fragment_shader)
 
   return
 }
 
-func LoadTriangle(program gl.Program) (gVAO gl.VertexArray) {
-  var gVBO gl.Buffer
-  var vertexData []gl.GLfloat
-  var attrib_loc gl.AttribLocation
+func LoadTriangle(program uint32) (gVAO uint32) {
+  var gVBO uint32
+  var vertexData []float32
+  var attrib_loc uint32
 
-  gVAO = gl.GenVertexArray()
-  gVAO.Bind()
+  gl.GenVertexArrays(1, &gVAO)
+  gl.BindVertexArray(gVAO)
 
-  gVBO = gl.GenBuffer()
-  gVBO.Bind(gl.ARRAY_BUFFER)
+  gl.GenBuffers(1, &gVBO)
+  gl.BindBuffer(gl.ARRAY_BUFFER, gVBO)
 
-  vertexData = []gl.GLfloat{
+  vertexData = []float32{
       //    x     y     z
           0.0,  0.8,  0.0,
          -0.8, -0.8,  0.0,
           0.8, -0.8,  0.0,
   }
 
-  gl.BufferData(gl.ARRAY_BUFFER, len(vertexData), vertexData, gl.STATIC_DRAW)
+  gl.BufferData(gl.ARRAY_BUFFER, len(vertexData)*4, gl.Ptr(vertexData), gl.STATIC_DRAW)
 
-  attrib_loc = program.GetAttribLocation("vert")
-  attrib_loc.EnableArray()
-  attrib_loc.AttribPointer(3, gl.FLOAT, false, 0, nil)
+  attrib_loc = uint32(gl.GetAttribLocation(program, gl.Str("vert\x00")))
+  gl.EnableVertexAttribArray(attrib_loc)
+  gl.VertexAttribPointer(attrib_loc, 3, gl.FLOAT, false, 0, nil)
 
-  gVBO.Unbind(gl.ARRAY_BUFFER)
+  gl.BindBuffer(gl.ARRAY_BUFFER, 0)
   clearVA()
 
   return
 }
 
 func clearVA() {
-  gl.VertexArray(0).Bind()
+  gl.BindVertexArray(0)
 }
 
-func Render(program gl.Program, gVAO gl.VertexArray) {
+func Render(window *glfw.Window, program uint32, gVAO uint32) {
   gl.ClearColor(0, 0, 0, 1)
   gl.Clear(gl.COLOR_BUFFER_BIT)
 
-  program.Use()
-  gVAO.Bind()
+  gl.UseProgram(program)
+  gl.BindVertexArray(gVAO)
   gl.DrawArrays(gl.TRIANGLES, 0, 3)
 
-  glfw.SwapBuffers()
+  window.SwapBuffers()
+  glfw.PollEvents()
 
   clearVA()
-  gl.ProgramUnuse()
+  gl.UseProgram(0)
 
 }
 
 func main() {
   glfw_init_err := glfw.Init()
-  if glfw_init_err != nil {
+  if !glfw_init_err {
     log.Fatal("GLFW: shit broke")
   }
   defer glfw.Terminate()
 
   screensize := Screen{800, 600}
 
-  glfw.OpenWindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-  glfw.OpenWindowHint(glfw.OpenGLVersionMajor, 3)
-  glfw.OpenWindowHint(glfw.OpenGLVersionMinor, 2)
-  glfw.OpenWindowHint(glfw.WindowNoResize, gl.TRUE)
+  glfw.WindowHint(glfw.Resizable, glfw.False)
+  glfw.WindowHint(glfw.ContextVersionMajor, 3)
+  glfw.WindowHint(glfw.ContextVersionMinor, 3)
+  glfw.WindowHint(glfw.OpenglForwardCompatible, glfw.True)
+  glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
+  glfw.WindowHint(glfw.OpenglDebugContext, glfw.True)
 
-  glfw_openwindow_err := glfw.OpenWindow(screensize.w, screensize.h, 8, 8, 8, 8, 0, 0, glfw.Windowed)
+  window, glfw_openwindow_err := glfw.CreateWindow(screensize.w, screensize.h, "I can't even remember what this program does", nil, nil)
   if glfw_openwindow_err != nil {
     log.Fatal("GLFW: could not open window")
   }
 
-  defer glfw.CloseWindow()
+
+  window.MakeContextCurrent()
 
   glew_init_err := gl.Init()
-  if glew_init_err != 0 {
+  if glew_init_err != nil {
     log.Fatal("GLEW: could not init")
   }
 
-  maj, min, _ := glfw.GLVersion()
-  if maj != 3 && min != 2 {
-    log.Fatal("GL: Couldn't get GL 3.2 core profile")
-  }
-
-  log.Printf("OpenGL Version: %s", gl.GetString(gl.VERSION))
-  log.Printf("GLSL Version: %s", gl.GetString(gl.SHADING_LANGUAGE_VERSION))
-  log.Printf("Vendor: %s", gl.GetString(gl.VENDOR))
-  log.Printf("Renderer: %s", gl.GetString(gl.RENDERER))
+  log.Printf("OpenGL Version: %s", gl.GoStr(gl.GetString(gl.VERSION)))
+  log.Printf("GLSL Version: %s", gl.GoStr(gl.GetString(gl.SHADING_LANGUAGE_VERSION)))
+  log.Printf("Vendor: %s", gl.GoStr(gl.GetString(gl.VENDOR)))
+  log.Printf("Renderer: %s", gl.GoStr(gl.GetString(gl.RENDERER)))
 
   program := LoadShaderProgram()
 
   gVAO := LoadTriangle(program)
 
-  for glfw.WindowParam(glfw.Opened) == gl.TRUE {
-    Render(program, gVAO)
+  for !window.ShouldClose() {
+    Render(window, program, gVAO)
   }
 
 }
